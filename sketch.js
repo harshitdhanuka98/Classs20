419e390aebcae8c72c45e41672f8e6eadccdae1f
harshitdhanuka98 <74587633+harshitdhanuka98@users.noreply.github.com>
2020-12-11 12:52:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect=createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor="red";
+  movingRect=createSprite(400, 200, 80, 50);
+  movingRect.shapeColor="red";
+}
+
+function draw() {
+  background(0);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2
+    && fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2
+    && movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2
+    && fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2){
+    movingRect.shapeColor="blue";
+    fixedRect.shapeColor="blue";
+  }
+  else{
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+  drawSprites();
+}
